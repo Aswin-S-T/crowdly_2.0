131d431ff8e28eb188826d90786b796ffb03bbb9
Aswin-S-T <aswinswebart15@gmail.com>
2023-04-07 12:12:12 +0530

    header done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ModeCommentIcon from "@mui/icons-material/ModeComment";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<header>
+				<nav class="navbar navbar-expand-lg navbar-light">
+					<a class="navbar-brand text-white" href="/">
+						Crowdly
+					</a>
+					<button
+						class="navbar-toggler"
+						type="button"
+						data-toggle="collapse"
+						data-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">
+									Home <span class="sr-only">(current)</span>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">
+									Link
+								</a>
+							</li>
+							<li class="nav-item dropdown">
+								<a
+									class="nav-link dropdown-toggle"
+									href="#"
+									id="navbarDropdown"
+									role="button"
+									data-toggle="dropdown"
+									aria-haspopup="true"
+									aria-expanded="false"
+								>
+									Dropdown
+								</a>
+								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+									<a class="dropdown-item" href="#">
+										Action
+									</a>
+									<a class="dropdown-item" href="#">
+										Another action
+									</a>
+									<div class="dropdown-divider"></div>
+									<a class="dropdown-item" href="#">
+										Something else here
+									</a>
+								</div>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link disabled" href="#">
+									Disabled
+								</a>
+							</li>
+						</ul>
+						<form class="form-inline my-2 my-lg-0">
+							<button
+								class="btn btn-outline-success my-2 my-sm-0"
+								type="submit"
+							>
+								Search
+							</button>
+						</form>
+					</div>
+				</nav>
+			</header>
+			<main>
+				<div className="row">
+					<div className="col-md-3">ds</div>
+					<div className="col-md-4">
+						<div className="post_section p-2">
+							<div className="card p-2">
+								<div className="post_header"></div>
+								<div className="post_image p-2">
+									<div>
+										<img
+											src="https://i.pinimg.com/736x/3f/e3/c4/3fe3c4455f467ef60b2129f2326150ea.jpg"
+											className="w-100"
+										/>
+									</div>
+								</div>
+								<hr />
+								<div className="post_actions">
+									<div>
+										0
+										<ThumbUpIcon />
+									</div>
+									<div>
+										0
+										<ModeCommentIcon />
+									</div>
+									<div>
+										0
+										<BookmarkBorderIcon />
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-3">rr</div>
+				</div>
+			</main>
+			<footer></footer>
+		</div>
+	);
 }
 
 export default App;

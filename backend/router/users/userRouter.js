c46eee7e2fdae0f4f73545a3d4ccad9063fe9d7e
Aswin-S-T <aswinswebart15@gmail.com>
2023-04-08 14:10:29 +0530

    all post api created

--- backend/router/users/userRouter.js
+++ backend/router/users/userRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const { getAllPost } = require("../../controllers/post/postController");
 const { register, login } = require("../../controllers/users/userController");
+const data = require("../../data");
 const { verifyToken } = require("../../middlewares/auth");
+const Post = require("../../models/post/postSchema");
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => {
+userRouter.get("/", async (req, res) => {
+	await Post.create(data.posts);
 	res.send("USER ROUTER CALLED");
 });
 
@@ -17,6 +21,12 @@ userRouter.post("/register", async (req, res) => {
 userRouter.post("/login", async (req, res) => {
 	const data = req.body;
 	login(data).then((response) => {
+		res.send(response);
+	});
+});
+
+userRouter.get("/all-post", async (req, res) => {
+	getAllPost().then((response) => {
 		res.send(response);
 	});
 });
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -6,6 +7,7 @@ const port = process.env.PORT || 5000;
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Database connection
 const db = require("./config/db");
